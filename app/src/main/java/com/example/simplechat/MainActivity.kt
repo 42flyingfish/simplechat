1ba4f819bf9bd17ef66c864fcd80f0d50611c427
Eric P <cire.ttalp@gmail.com>
2024-12-11 02:10:36 -0800

    Simple interaction.

--- app/src/main/java/com/example/simplechat/MainActivity.kt
+++ app/src/main/java/com/example/simplechat/MainActivity.kt
@@ -3,15 +3,18 @@ package com.example.simplechat
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.simplechat.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var mAuth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,6 +26,14 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        startActivity(Intent(this, SigninActivity::class.java))
+        mAuth = FirebaseAuth.getInstance()
+
+        if (mAuth.currentUser == null)
+            startActivity(Intent(this, SigninActivity::class.java))
+
+
+        Toast.makeText(baseContext,
+            "You made it ${mAuth.currentUser?.email}",
+            Toast.LENGTH_LONG).show()
     }
 }
