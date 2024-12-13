d8265fc1ea9bb82efad6bc22a161465da025aaeb
Eric P <cire.ttalp@gmail.com>
2024-12-12 16:57:46 -0800

    Recycler updates from the database.

--- app/src/main/java/com/example/simplechat/ChatMessageAdapter.kt
+++ app/src/main/java/com/example/simplechat/ChatMessageAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.simplechat
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +10,6 @@ import androidx.recyclerview.widget.RecyclerView
 class ChatMessageAdapter(private val dataSet: MutableList<Message>) :
     RecyclerView.Adapter<ChatMessageAdapter.ViewHolder>() {
         inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            // TODO("Implement the thing")
             val messageTextView: TextView = itemView.findViewById(R.id.mesageBox)
             val senderTextView: TextView = itemView.findViewById(R.id.senderBox)
 
@@ -29,7 +29,41 @@ class ChatMessageAdapter(private val dataSet: MutableList<Message>) :
     }
 
     override fun getItemCount(): Int {
+        Log.d("getItemCount", "Item count is ${dataSet.size}")
         return dataSet.size
     }
+
+    // Update the adapter with new data
+
+    fun updateMessages(newMessages: MutableList<Message>) {
+        Log.d("Database Replacement", "updateMessages  The dataSet contained: ${dataSet.size}")
+        Log.d("Database Replacement", "updateMessages The newMessages contained: ${newMessages.size}")
+        Log.d("Database Replacement", "updateMessages The newMessages contained: ${newMessages}")
+        if (newMessages.isEmpty()) {
+            Log.d("updateMessages", "updateMessages The newMesages is somehow empty prior to clearing the db")
+            return
+        }
+        dataSet.clear()
+        //val success = dataSet.addAll(newMessages)
+//        if (!success) {
+//            Log.d("Database Replacement", "updateMessages The dataset failed to change")
+//        }
+        if (newMessages.isEmpty()) {
+            Log.d("updateMessages", "updateMessages The newMesages is somehow empty after calling clear")
+            return
+        }
+        dataSet.add(newMessages.first())
+        Log.d("Database Replacement", "udateMessages The dataSet now has : ${dataSet.size}")
+        notifyDataSetChanged()
+    }
+
+    fun clearList(newMessages: MutableList<Message>) {
+        newMessages.clear()
+    }
+
+    fun forceRefresh() {
+        notifyDataSetChanged()
+    }
+
 
 }
--- app/src/main/java/com/example/simplechat/MainActivity.kt
+++ app/src/main/java/com/example/simplechat/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.simplechat
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -11,8 +12,11 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.simplechat.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -22,6 +26,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     private lateinit var mAuth : FirebaseAuth
     private lateinit var mData : DatabaseReference
+    private lateinit var mDataWatch: DatabaseReference
     private lateinit var adapter: ChatMessageAdapter
     private val messages = mutableListOf<Message>()
 
@@ -49,7 +54,7 @@ class MainActivity : AppCompatActivity() {
             Toast.LENGTH_LONG).show()
 
         mData = Firebase.database.reference
-
+        mDataWatch = FirebaseDatabase.getInstance().getReference("messages")
         val database = Firebase.database
 
 
@@ -61,6 +66,39 @@ class MainActivity : AppCompatActivity() {
         adapter = ChatMessageAdapter(messages)
         binding.recycle.layoutManager = LinearLayoutManager(this)
         binding.recycle.adapter = adapter
+
+        // Listen for changes in the database
+        // Notice that the messages is passed by reference to the adapter.
+        // We can update the adapter by updating messages directly and then
+        // calling forceRefresh
+        // This is not a good idea for encapsilation.
+        mDataWatch.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // check before clearing
+                Log.d("Messages", "addValueEventListener Fetched messages: $messages")
+                // Clear the existing messages
+                messages.clear()
+                // Read messages from the database
+                for (messageSnapshot in dataSnapshot.children) {
+                    val message = messageSnapshot.getValue(Message::class.java)
+                    if (message != null) {
+                        messages.add(message)
+                    }
+                }
+                Log.d("Messages", "addValueEventListener Fetched messages: $messages")
+                // Update the adapter with the new messages
+                Log.d("Database change", "addValueEventListener The dataset now contains ${messages.size}")
+                //adapter.updateMessages(messages)
+                adapter.forceRefresh()
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle errors
+                Log.d("DBERROR", "onCancelled: ${databaseError.toString()}")
+            }
+        })
+
+
 
 
 
